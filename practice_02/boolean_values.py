40d2e57590fc3b56e2630db106a95dceae4ba1d1
svtqza <saruzhan008@gmail.com>
2026-02-03 15:09:35 +0500

    Clean up boolean values demonstration code
    Remove commented-out code that demonstrates boolean values.

--- practice_02/boolean_values.py
+++ practice_02/boolean_values.py
@@ -11,19 +11,3 @@ if b > a:
   print("b is greater than a")
 else:
   print("b is not greater than a")
-
-#3
-print(bool("Hello"))
-print(bool(15))
-
-#4
-x = "Hello"
-y = 15
-
-print(bool(x))
-print(bool(y))
-
-#5
-bool("abc")
-bool(123)
-bool(["apple", "cherry", "banana"])
